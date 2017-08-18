9c6b6034148109fd328e3a235ac790494ba54c99
AlexMollohan <ajmollohan@gmail.com>
2017-08-17 20:32:10 -0400

    Turned search bar component into state-tied component and bound this into the input change method

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = { term: '' }
+
+    // This line is needed in the constructor to bind the component level this to the onInputChange method
+    // Context almost alwyas needs to be bound if you are passing callbacks
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
+    this.setState({term:event.target.value})
+  }
+
   render() {
     return(
       <form className="input-group">
-        <input/>
+        <input
+          placeholder="Get a Five Day Forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+          />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
